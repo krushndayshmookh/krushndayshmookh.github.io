bb5771f532925764eff3f23449b9023bb98f8871
krushndayshmookh <krushndayshmookh@gmail.com>
2017-05-04 15:08:42 +0530

    Redesign Complete

--- js/init.js
+++ js/init.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
 	$('.button-collapse').sideNav();
 	$('.parallax').parallax();
 
-	$("#panel-0").slideUp();
-
+	$("#panel-0").slideUp("fast", addAnimationClass("#panel-back-1", "panel-back-animated"));
+	//$("#panel-back-1").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",$("#scrol-btn-1").addClass("animated"));
 
 
 
@@ -12,3 +12,14 @@ $(document).ready(function () {
 
 
 }); // end of document ready
+
+
+
+
+
+
+
+
+function addAnimationClass(element, animation) {
+	$(element).addClass(animation);
+}
